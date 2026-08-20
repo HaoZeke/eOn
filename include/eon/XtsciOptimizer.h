322072b42135e207f5b3310cf4630d98a0c38c53
Rohit Goswami <rog32@hi.is>
2026-08-20 05:29:32 -0500

    fix(optimizer): use current eOn optimizer config

--- include/eon/XtsciOptimizer.h
+++ include/eon/XtsciOptimizer.h
@@ -14,7 +14,7 @@ class XtsciOptimizer final : public Optimizer {
 public:
   XtsciOptimizer(std::shared_ptr<ObjectiveFunction> a_objf,
                  const Parameters &a_params)
-      : Optimizer(a_objf, OptType::XTSCI, a_params) {}
+      : Optimizer(a_objf, OptType::XTSCI, OptimizerConfig::fromParams(a_params)) {}
 
   int step(double a_maxMove) override;
   int run(size_t a_maxIterations, double a_maxMove) override;
